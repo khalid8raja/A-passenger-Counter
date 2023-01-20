24af5624015a7b977beb6e2ebbefbadb70d08974
khalid8raja <105552193+khalid8raja@users.noreply.github.com>
2023-01-20 18:43:41 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+let saveEl = document.getElementById("save-el") // previous : 
+let countEl = document.getElementById("count-el") // counting nubmer
+let count = 0
+
+function increment() {
+    count += 1
+    countEl.textContent = count
+}
+
+function save() {
+    let countStr = count + " - "
+    saveEl.textContent += countStr
+    countEl.textContent = 0
+    count = 0
+}
